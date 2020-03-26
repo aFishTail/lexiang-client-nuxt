6de19fe0d53d8328b3655be6395661e9b0d6ce38
unknown <you@example.com>
2020-03-26 23:25:12 +0800

    first

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="home">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.home{
+  width: 100vw;
+  height: 100vh;
+  background: url('~assets/imgs/home_bg.png');
+}
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <p>欢迎进入登录页面</p>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.home{
+  width: 100vw;
+  height: 100vh;
+  background: url('~assets/imgs/home_bg.png');
+}
+</style>
